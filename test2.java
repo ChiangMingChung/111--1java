d555856fd50bed84bf0e021335c632a55ba53bb5
ChiangMingChung <114204221+ChiangMingChung@users.noreply.github.com>
2022-09-30 11:51:10 +0800

    0930-java

---
+++ test2.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Test2{
+    public static void main(String[] args){
+    	int num1;
+		
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter first integer:");
+        num1 = input.nextInt();
+        
+        if(num1>=60 & num1 <=70){
+            System.out.println("<=70");
+            }
+        }
+        Else{
+            System.out.println(">70");
+        }
+    }
