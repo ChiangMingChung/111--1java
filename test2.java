1e4553b13226c0d82fdfdffdee04309aaa7566f5
ChiangMingChung <114204221+ChiangMingChung@users.noreply.github.com>
2022-10-07 09:06:27 +0800

    for teacher 9:30

--- test2.java
+++ test2.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Test2{
+public class test2{
     public static void main(String[] args){
     	int num1;
 		
@@ -8,11 +8,20 @@ public class Test2{
         System.out.println("Enter first integer:");
         num1 = input.nextInt();
         
-        if(num1>=60 & num1 <=70){
-            System.out.println("<=70");
+        if(num1>=90 & num1<=100){
+            System.out.println("A");
             }
+            else if(num1>=80 & num1<=90){
+                System.out.println("B");
+            }
+            else if(num1>=70 & num1<=80){
+                System.out.println("C");
+            }
+            else if(num1>=60 & num1<=70){
+                System.out.println("D");
         }
-        Else{
-            System.out.println(">70");
+        else{
+            System.out.println("E");
         }
     }
+}
