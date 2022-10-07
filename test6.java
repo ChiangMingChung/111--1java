07f5c7e746d97a58a18f49051fd6ddf583d36f83
ChiangMingChung <114204221+ChiangMingChung@users.noreply.github.com>
2022-10-07 11:40:34 +0800

    for teacher

---
+++ test6.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class test6{
+    public static void main(String[] args){
+        int num1 = 0;
+        String space = "";
+        
+        while(num1 < 10){
+            System.out.printf("%s%d %n",space,num1);
+            num1 = num1 + 1;
+            space = space + " ";
+            
+        }
+    
+    }
+}
+        
+            
+        
