b3214fd026e8156e536d86b23b8b3192d8efc55b
ChiangMingChung <114204221+ChiangMingChung@users.noreply.github.com>
2022-10-28 11:15:11 +0800

    Add files via upload

---
+++ test10.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class test10{
+	public static void main(String args[]){
+		for(int i = -1;i >-7;i--){
+			for(int k =-6;k<0;k++){
+				if(k<i)
+					System.out.print("*");
+					
+				else
+					System.out.printf("%d",k-i+1);
+						
+			}
+			System.out.println("");
+	    }
+	}
+}
+			 
+		
+		
+			
+	
+	
