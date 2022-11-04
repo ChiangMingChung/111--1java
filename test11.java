3ab2583848c9b6f63dcb555ae42d9eb4067b0c82
ChiangMingChung <114204221+ChiangMingChung@users.noreply.github.com>
2022-11-04 11:31:52 +0800

    Add files via upload

---
+++ test11.java
@@ -0,0 +1,14 @@
+public class Test11 {
+	public static void main(String agrs[]){
+		float a =0.0f;
+		float b =0.2f;
+		
+		a +=0.1;
+		a +=0.1;
+		
+		if(a == b)
+			System.out.printf("a == b");
+		else
+			System.out.printf("a != b");
+	}
+}
